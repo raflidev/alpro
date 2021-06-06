3f3bf0e1a935e348e83403bc2b2bf3511f2ad724
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-06-06 23:06:46 +0700

    pseudocode

--- clo4/sort/tugas_m12/3.go
+++ clo4/sort/tugas_m12/3.go
@@ -1,76 +1,76 @@
-// package main
+package main
 
-// import "fmt"
+import "fmt"
 
-// const NMAX = 60
+const NMAX = 60
 
-// type bunga struct {
-// 	nama    string
-// 	panjang int
-// }
+type bunga struct {
+	nama    string
+	panjang int
+}
 
-// type tabBunga = [NMAX]bunga
+type tabBunga = [NMAX]bunga
 
-// func panjang(bunga string) int {
-// 	var result int
-// 	i := 0
-// 	for string([]rune(bunga)[i]) != "." {
-// 		bungatab := string([]rune(bunga)[i])
-// 		if bungatab != "_" {
-// 			result++
-// 		}
-// 		i++
-// 	}
-// 	return result
+func panjang(bunga string) int {
+	var result int
+	i := 0
+	for string((bunga)[i]) != "." {
+		bungatab := string((bunga)[i])
+		if bungatab != "_" {
+			result++
+		}
+		i++
+	}
+	return result
 
-// }
+}
 
-// func isiArray(b *tabBunga, n *int) {
-// 	var bunga string
-// 	fmt.Scanln(*&n)
-// 	for i := 0; i < *n; i++ {
-// 		fmt.Scanln(&bunga)
-// 		b[i].nama = bunga
-// 		b[i].panjang = panjang(bunga)
-// 	}
-// }
+func isiArray(b *tabBunga, n *int) {
+	var bunga string
+	fmt.Scanln(*&n)
+	for i := 0; i < *n; i++ {
+		fmt.Scanln(&bunga)
+		b[i].nama = bunga
+		b[i].panjang = panjang(bunga)
+	}
+}
 
-// func mengurutkan(b *tabBunga, n int) {
-// 	var pass, i int
-// 	var temp bunga
-// 	pass = 1
-// 	for pass <= n-1 {
-// 		i = pass
-// 		temp = b[pass]
-// 		for i > 0 && temp.panjang < b[i-1].panjang {
-// 			b[i] = b[i-1]
-// 			i--
-// 		}
-// 		b[i] = temp
-// 		pass++
-// 	}
-// }
+func mengurutkan(b *tabBunga, n int) {
+	var pass, i int
+	var temp bunga
+	pass = 1
+	for pass <= n-1 {
+		i = pass
+		temp = b[pass]
+		for i > 0 && temp.panjang < b[i-1].panjang {
+			b[i] = b[i-1]
+			i--
+		}
+		b[i] = temp
+		pass++
+	}
+}
 
-// func tampilArray(b tabBunga, n int) {
-// 	for i := 0; i < n; i++ {
-// 		fmt.Println(b[i].nama)
-// 	}
-// }
+func tampilArray(b tabBunga, n int) {
+	for i := 0; i < n; i++ {
+		fmt.Println(b[i].nama)
+	}
+}
 
-// func main() {
-// 	var b tabBunga
-// 	var n int
-// 	isiArray(&b, &n)
-// 	mengurutkan(&b, n)
-// 	fmt.Println("")
-// 	tampilArray(b, n)
-// }
+func main() {
+	var b tabBunga
+	var n int
+	isiArray(&b, &n)
+	mengurutkan(&b, n)
+	fmt.Println("")
+	tampilArray(b, n)
+}
 
-// // 7
-// // Mawar.
-// // Lili.
-// // Kertas.
-// // Forget_me_not.
-// // Kamboja.
-// // Anggrek.
-// // Rafflesia.
+// 7
+// Mawar.
+// Lili.
+// Kertas.
+// Forget_me_not.
+// Kamboja.
+// Anggrek.
+// Rafflesia.
